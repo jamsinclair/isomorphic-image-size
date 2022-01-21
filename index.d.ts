3e462cb3a1376624c57bf657153b6b6b0e0be850
Jamie Sinclair <jamsinclairnz@gmail.com>
2022-01-21 13:03:20 +0900

    fix: update return types

--- index.d.ts
+++ index.d.ts
@@ -7,4 +7,4 @@ type SizeResult = {
     hUnits: string;
 }
 
-export default function get_image_size(data: Array | ArrayBuffer, ext: string): SizeResult;
+export default function get_image_size(data: Array | ArrayBuffer, ext: string): SizeResult | null;
